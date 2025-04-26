1b68d0a858845feb7e271b4a9fa40a773f45f69f
Ruslan543 <rusel45612@gmail.com>
2025-04-26 14:19:47 +0300

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,9 @@ const HomePage: NextPage<any> = () => {
   return (
     <div className="text-center h-screen flex items-center justify-center">
       Привет! Это тестовое задание на проверку REST API :)
+      <br />
+      Если запросы к REST API выполняются долго - это значит, что приостановился
+      сервер из-за бездействия. Нужно немного подождать
     </div>
   );
 };
